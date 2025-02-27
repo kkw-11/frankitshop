b1bde3b7f0436d11183fe1ea36ec3cf32749248a
kiwon <kkw-11@naver.com>
2025-02-27 14:09:41 +0900

    first commit

---
+++ src/main/java/com/shop/frankit/FrankitApplication.java
@@ -0,0 +1,13 @@
+package com.shop.frankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/frankit/FrankitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.frankit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
