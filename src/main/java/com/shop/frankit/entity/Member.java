bedc1ba3bcd430f98bebb707b4bc3e98eb1b1d45
kiwon <kkw-11@naver.com>
2025-02-28 16:52:24 +0900

    feat: add Member entity class

---
+++ src/main/java/com/shop/frankit/entity/Member.java
@@ -0,0 +1,28 @@
+package com.shop.frankit.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Table(name = "members")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Setter
+    private String email;
+
+    @Column(nullable = false)
+    @Setter
+    private String password;
+}
