2b96b17c16e203f1a4a3a7a39b1a8904331d4432
kiwon <kkw-11@naver.com>
2025-02-28 16:53:08 +0900

    feat: add BaseEntity class

---
+++ src/main/java/com/shop/frankit/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.shop.frankit.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class BaseEntity {
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createTime;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime updateTime;
+}
