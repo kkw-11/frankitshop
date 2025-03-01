e71d3ff91c058d9d6666ea9ce9eb6449a420815f
kiwon <kkw-11@naver.com>
2025-03-01 14:36:14 +0900

    feat:implement UserDetailsImpl for custom UserDetails Class

---
+++ src/main/java/com/shop/frankit/security/UserDetailsImpl.java
@@ -0,0 +1,70 @@
+package com.shop.frankit.security;
+
+import com.shop.frankit.dto.UserDTO;
+import com.shop.frankit.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Data
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String role;
+
+    public static UserDetailsImpl build(User user) {
+        return new UserDetailsImpl(
+            user.getId(),
+            user.getEmail(),
+            user.getPassword(),
+            user.getRole()
+        );
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // 이메일을 사용자명으로 사용
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public UserDTO toDTO() {
+        return new UserDTO(id, email, role);
+    }
+}
