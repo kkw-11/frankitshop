939416ae97138f2ac035eb3050c2582c180a49ab
kiwon <kkw-11@naver.com>
2025-03-01 14:47:39 +0900

    feat:add UserDTO

---
+++ src/main/java/com/shop/frankit/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package com.shop.frankit.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * UserDTO는 User 엔티티의 정보를 클라이언트에 전달할 때 사용하는 객체입니다. 보안상 비밀번호는 포함하지 않습니다.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long id;
+    private String username;
+    private String role;
+
+}
